a217c8ab26b93444f2f2a3ae073500518a9c137e
Kun-Yuan Lee <54857031+klee12@users.noreply.github.com>
2021-09-10 14:54:57 -0500

    update
    test

--- practiceInClass.R
+++ practiceInClass.R
@@ -1,3 +1,9 @@
 #This is the practice lab
 #Hello 
+p.counties <- "./data/CBW/County_Boundaries.shp"
+p.stations <- "./data/CBW/Non-Tidal_Water_Quality_Monitoring_Stations_in_the_Chesapeake_Bay.shp"
 
+d.counties <- sf::read_sf(p.counties)
+d.stations <- sf::read_sf(p.stations)
+
+glimpse(d.counties)
